e012e9e75f59a9b1fe273ed4e3e79df52203a462
sbkgladiator <g.gladiator@tiscali.it>
2013-04-02 17:02:04 +0200

    esercizi

---
+++ solidi.js
@@ -0,0 +1,7 @@
+
+var l1=POLYLINE([[7,1],[1,1],[1,12],[7,12],[7,9]]);
+var l2=POLYLINE([[5,7],[15,7]]);
+var l3=POLYLINE([[14,5],[20,5]]);
+var l4=POLYLINE([[23,8],[28,8]]);
+var linee=STRUCT([l1,l2,l3,l4]);
+DRAW(linee)
