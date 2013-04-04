4bce36244d5f2b4d8111afbbaf96d8504d9753d2
sbkgladiator <g.gladiator@tiscali.it>
2013-04-04 18:33:53 +0200

    edificio

--- grafica04042013.py
+++ grafica04042013.py
@@ -4,7 +4,7 @@ from pyplasm import *
 
 foundations = INSR(PROD)(AA(QUOTE)([[8,-30,8,-30,8,-12],[8,-30,8],[6]]))
 
-VIEW(foundations)
+
 
 pillars = INSR(PROD)(AA(QUOTE)([[-3,2,-36,2,-36,2,-18,2],[-3,2,-36,2],[-7.4,1.4,23.6,1.4,23.6,1.4]]))
 
@@ -20,15 +20,14 @@ step2D = MKPOL([[[0,0],[0,2.65],[2.66,2.5/2],[2.66,2.65]],[[1,2,3,4]],None])
 
 ###gradino in 3D 
 step3D = MAP([S1,S3,S2])(PROD([step2D,Q(9)]))
-
-
-
 ramp = STRUCT(NN(10)([step3D,T([1,3])([2.66,2.5/2])]))
 
 
 ramp1 = T([1,2,3])([3+2+36+2+36+2+18,3+2+12,6])(R([1,2])(PI/2)(ramp))
 
+ramp2 = T([1,2,3])([3+2+36+2+36+2,3+2+12+2.66*10,6+1.25*9])(R([1,2])(-PI/2)(ramp))
+pian=INSR(PROD)(AA(QUOTE)([[-3,-2,-36,-2,-36,-2,18],[-17,-2.66*10,6],[-6,-1.25*10,1.4]]))
+scala=T([3])([6+1.25*15])(STRUCT([ramp1,ramp2,pian]))
+building = STRUCT([foundations,pillars,T(2)(-6)(horiz_partitions),ramp1,ramp2,pian,scala])
 
-building = STRUCT([foundations,pillars,T(2)(-6)(horiz_partitions),ramp1])
-
-###VIEW(building)
+VIEW(building)
