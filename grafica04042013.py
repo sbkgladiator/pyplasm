636c803abe9c918c52a7f41d2eb0b53d94b1dcfb
sbkgladiator <g.gladiator@tiscali.it>
2013-04-04 18:06:53 +0200

    esercizi

---
+++ grafica04042013.py
@@ -0,0 +1,34 @@
+from pyplasm import *
+
+###quote sulla direzione degli assi x,y,z
+
+foundations = INSR(PROD)(AA(QUOTE)([[8,-30,8,-30,8,-12],[8,-30,8],[6]]))
+
+VIEW(foundations)
+
+pillars = INSR(PROD)(AA(QUOTE)([[-3,2,-36,2,-36,2,-18,2],[-3,2,-36,2],[-7.4,1.4,23.6,1.4,23.6,1.4]]))
+
+
+horiz_partitions = INSR(PROD)(AA(QUOTE)([[-3,2,36,2,36,2,18,2],[9,2,36,2,9],[-6,1.4,-23.6,1.4,-23.6,1.4]]))
+
+
+building = STRUCT([foundations,pillars,T(2)(-6)(horiz_partitions)])
+
+
+###gradino in 2D
+step2D = MKPOL([[[0,0],[0,2.65],[2.66,2.5/2],[2.66,2.65]],[[1,2,3,4]],None])
+
+###gradino in 3D 
+step3D = MAP([S1,S3,S2])(PROD([step2D,Q(9)]))
+
+
+
+ramp = STRUCT(NN(10)([step3D,T([1,3])([2.66,2.5/2])]))
+
+
+ramp1 = T([1,2,3])([3+2+36+2+36+2+18,3+2+12,6])(R([1,2])(PI/2)(ramp))
+
+
+building = STRUCT([foundations,pillars,T(2)(-6)(horiz_partitions),ramp1])
+
+###VIEW(building)
