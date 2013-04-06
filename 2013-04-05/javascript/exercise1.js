1225d713142a512e83a2e3ce8434c58ea1ac74f4
sbkgladiator <g.gladiator@tiscali.it>
2013-04-06 09:28:19 +0200

    hw1

--- 2013-04-05/javascript/exercise1.js
+++ 2013-04-05/javascript/exercise1.js
@@ -65,8 +65,17 @@ var pillars3=STRUCT([pilla14,pilla24]);
 
 
 
+var east=T([1])([a.y+12]) (R([1,2])(PI/2) (STRUCT([SIMPLEX_GRID([[-364,b.x-364],[-h,195-h,-243+195,313-243,-362+313,431-362,-483+431,532-483],[12]]),SIMPLEX_GRID([[-a.x,359-a.x],[-h,429-h,-483+429,532-483],[12]]),SIMPLEX_GRID([[-354,366-354,-492+366,b.x-492],[-h,534-h],[12]]) ])));
 
-var building=STRUCT([pillars0,pillars1,pillars2,pillars3,floor1,floor0,floor2,floor3,floor4])
+var north=T([0])([b.x]) ( R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-a.y,d.y-a.y],[12]])));
+var south=T([0])([a.x]) (R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-a.y,d.y-a.y],[12]])));
+var west=T([1])([d.y]) (R([1,2])(PI/2)(SIMPLEX_GRID([[-a.x,b.x-a.x],[-h,530-h],[12]])));
+
+
+
+
+
+var building=STRUCT([pillars0,pillars1,pillars2,pillars3,floor1,floor0,floor2,floor3,floor4,east])
 
 
 DRAW(building)
