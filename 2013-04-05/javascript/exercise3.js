fe42c110bf9a852b135840bba6a33149745f5e0c
sbkgladiator <g.gladiator@tiscali.it>
2013-04-06 08:16:23 +0200

    hw1

--- 2013-04-05/javascript/exercise3.js
+++ 2013-04-05/javascript/exercise3.js
@@ -1,5 +1,4 @@
-var east=R([1,2])(PI/2)(SIMPLEX_GRID([[-p1.x,p5.x-p1.x],[-h,530-h]]));
-var north=R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-p1.y,q7.y-p1.y]]));
-var south=T([0])([p5.x])(R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-p1.y,q7.y-p1.y]])));
-var west=T([1])([q7.y])(R([1,2])(PI/2)(SIMPLEX_GRID([[-p1.x,p5.x-p1.x],[-h,530-h]])));
-
+var east=T([1])([a.y+12]) (R([1,2])(PI/2) (SIMPLEX_GRID([[-a.x,b.x-a.x],[-h,530-h],[12]])));
+var north=T([0])([b.x]) ( R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-a.y,d.y-a.y],[12]])));
+var south=T([0])([a.x]) (R([0,2])(-PI/2)(SIMPLEX_GRID([[-h,530-h],[-a.y,d.y-a.y],[12]])));
+var west=T([1])([d.y]) (R([1,2])(PI/2)(SIMPLEX_GRID([[-a.x,b.x-a.x],[-h,530-h],[12]])));
